0091a80a6fe7d4046aa88cc4994138af1cdef3f4
robertolatini31 <robertolatini31@gmail.com>
2022-07-01 17:03:40 +0200

    fix css admin messages show

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -8,11 +8,9 @@
         <strong>Oggetto:</strong> {{$message->subject}} <br>
         <strong>Email:</strong> {{$message->email}}
         <p class="bg-secondary p-2 mt-3 rounded">
-            <strong class="mr-2">Testo:</strong> 
+            <strong class="mr-2">Testo:</strong>
             <span>{{$message->message}}</span>
         </p>
     </div>
-
-
 </div>
 @endsection
