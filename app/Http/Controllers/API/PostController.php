d26044cf4f0ed6a49cf0ca16ed0acac1c5f1e5c5
robertolatini31 <robertolatini31@gmail.com>
2022-08-01 12:33:57 +0200

    fix

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -21,6 +21,7 @@ class PostController extends Controller
                 'status_code' => 404,
                 'status_text' => 'not found'
             ]);
+            
         }
        
     }
